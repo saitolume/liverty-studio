cf524995a59a3d55861ac8c1d89077e44d5e02cf
Tadao Iseki <saitoeku3@gmail.com>
2019-12-08 00:19:21 +0900

    Change theme colors

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,7 +1,8 @@
 export const theme = {
-  grayDark: '#1a1d21',
-  grayLight: '#35373b',
-  white: '#ddd',
+  grayDark: '#222',
+  gray: '#383838',
+  grayLight: '#5a5a5a',
+  white: '#fff',
   primary: '#34785c',
   primaryDark: '#204a38'
 }
