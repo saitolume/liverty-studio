b09cd9472f481dc53c5fa6fe5db89b8f0a07943c
Tadao Iseki <saitoeku3@gmail.com>
2019-12-09 15:28:03 +0900

    Add Mixer component

--- src/renderer/components/MenuMixer.tsx
+++ src/renderer/components/MenuMixer.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import MenuBase from './MenuBase'
+import Mixer from './Mixer'
 
-const MenuMixer: React.FC = () => <MenuBase title="Mixer"></MenuBase>
+const MenuMixer: React.FC = () => {
+  return (
+    <Wrapper title="Mixer">
+      <Mixer />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled(MenuBase)`
+  padding: 12px 12px 0;
+  width: calc(100% - 32px);
+  height: calc(100% - 40px);
+`
 
 export default MenuMixer
