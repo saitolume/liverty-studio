87746723a4cd1f31b7e33778b77a8f4837d65674
Tadao Iseki <saitoeku3@gmail.com>
2019-12-09 23:02:30 +0900

    Add hook to use microphone

---
+++ src/renderer/hooks/useMicrophone.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
+
+export type Microphone = {
+  deviceName: string
+  audioTrack: MediaStreamTrack | null
+}
+
+export const useMicrophone = () => {
+  const [audioTrack, setAudioTrack] = useState<MediaStreamTrack | null>(null)
+  const [deviceName, setDeviceName] = useState('')
+
+  const microphone = useMemo<Microphone>(() => ({ audioTrack, deviceName }), [
+    audioTrack,
+    deviceName
+  ])
+
+  const getMicrophone = useCallback(async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    const [{ deviceId, label }] = devices.filter(device => device.kind === 'audioinput')
+    setDeviceName(label)
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: { deviceId },
+      video: false
+    })
+    const [audioTrack] = stream.getAudioTracks()
+    setAudioTrack(audioTrack)
+  }, [])
+
+  useEffect(() => {
+    ;(async () => {
+      await getMicrophone()
+    })()
+  }, [getMicrophone])
+
+  return microphone
+}
